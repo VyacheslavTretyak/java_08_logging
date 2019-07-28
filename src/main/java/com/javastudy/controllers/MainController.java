cdbf847c72400052baae5c2640004ebb6d24fde1
vtretyak <tretyak1c@gmail.com>
2019-07-28 18:13:56 +0300

    add: logger

--- src/main/java/com/javastudy/controllers/MainController.java
+++ src/main/java/com/javastudy/controllers/MainController.java
@@ -1,18 +1,19 @@
 package com.javastudy.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.logging.Logger;
+
 
 @Controller
 public class MainController {
 
-    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+    private static final Logger logger = Logger.getLogger(MainController.class.toString());
 
     @RequestMapping(value = "/second")
     public String indexPage() {
@@ -26,6 +27,9 @@ public class MainController {
 
     @RequestMapping(value ="/welcome", method = RequestMethod.GET)
     public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+        logger.warning("LOG: Call method \"login\" of MainController class");
+        System.out.println("Call method \"login\" of MainController class");
+
         ModelAndView model = new ModelAndView();
         if (error != null) {
             model.addObject("error", "Invalid username or password!");
